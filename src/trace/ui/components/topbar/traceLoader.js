54bf494884415a90563a4c89967a76ed4932db36
Yeyi He <81464025+hoyipyik@users.noreply.github.com>
2025-03-02 00:04:06 +0100

    Add traceLoader for handling trace file uploads and parsing

---
+++ src/trace/ui/components/topbar/traceLoader.js
@@ -0,0 +1,31 @@
+import { callTreeParser } from "../../../utils/process/callTreeParser.js";
+import { xmlFileReader } from "../../../utils/process/xmlFileReader.js";
+import { rerender } from "../../views/index.js";
+
+export const traceLoader = () => {
+    document.addEventListener('DOMContentLoaded', function () {
+        console.log('Trace file loader initialized');
+        const traceButton = document.getElementById('trace-btn');
+        const fileInput = document.getElementById('call-tree-selector');
+
+        // Add event listener to the button
+        traceButton.addEventListener('click', () => {
+            fileInput.click(); // Trigger the file input click
+            console.log('Button clicked');
+        });
+
+        // Add event listener to the file input
+        fileInput.addEventListener('change', async (event) => {
+            const file = event.target.files[0];
+            if (file) {
+                const parsedXml = await xmlFileReader(file);
+                // console.log("context", window.context);
+                const { nodes: parsedNodes, edges: parsedEdges, style } = callTreeParser(parsedXml);
+                const graph = { nodes: parsedNodes, edges: parsedEdges, style };
+                window.graph = graph;
+                rerender(true);
+                // console.log("parseXML", parsedXml);
+            }
+        });
+    });
+}
