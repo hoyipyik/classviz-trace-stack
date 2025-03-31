ed2a8d687f0a2924329cc9da4a817b83226be3c3
Yeyi He <81464025+hoyipyik@users.noreply.github.com>
2025-03-31 14:11:29 +0200

    feat: add constants for reusable values in contants.js

---
+++ src/flame/services/contants.js
@@ -0,0 +1,14 @@
+// Constants for reusable values
+export const CONSTANTS = {
+    CELL_HEIGHT: 18,
+    TRANSITION_DURATION: 750,
+    MIN_FRAME_SIZE: 1,
+    CHART_SELECTOR: '#chart',
+    CONTAINER_ID: 'flame-container',
+    MIN_HEIGHT: 10,
+    STATES: {
+      MINIMIZED: 'minimized',
+      SCROLLABLE: 'scrollable',
+      EXPANDED: 'expanded'
+    }
+  };
