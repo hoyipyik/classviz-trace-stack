d8bec5b7685e7dcfa586bd557e0fc5f125d31967
Yeyi He <81464025+hoyipyik@users.noreply.github.com>
2025-04-04 14:47:55 +0200

    change: change the constants

--- src/flame/services/contants.js
+++ src/flame/services/contants.js
@@ -2,13 +2,13 @@
 export const CONSTANTS = {
     CELL_HEIGHT: 18,
     TRANSITION_DURATION: 750,
-    MIN_FRAME_SIZE: 1,
+    MIN_FRAME_SIZE: 0.001,
     CHART_SELECTOR: '#chart',
     CONTAINER_ID: 'flame-container',
+    SIDEBAR_ID: 'flame-sidebar-container',
     MIN_HEIGHT: 10,
     STATES: {
       MINIMIZED: 'minimized',
-      SCROLLABLE: 'scrollable',
       EXPANDED: 'expanded'
     }
   };
