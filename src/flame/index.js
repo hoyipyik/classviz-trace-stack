2c6bbd4cd04011bd3904f58617ded811493faab8
Yeyi He <81464025+hoyipyik@users.noreply.github.com>
2025-04-06 14:11:13 +0200

    clean: clean code

--- src/flame/index.js
+++ src/flame/index.js
@@ -3,12 +3,12 @@
  * A collection of functions for managing the flame graph visualization with a draggable container
  */
 
+import { updateTraceNodesOnClassviz } from "../trace/utils/traceNodeOnClassviz/nodeManager.js";
 import { FlameGraphService } from "./services/flameGraphMain.js";
-
 // Export the flame graph plugin loader
 export const loadFlameGraphPlugin = () => {
     const service = new FlameGraphService();
-    
+    // updateTraceNodesOnClassviz();
     // Use 'DOMContentLoaded' for better performance than document.readyState check
     if (document.readyState === 'loading') {
         document.addEventListener("DOMContentLoaded", service.initialize);
