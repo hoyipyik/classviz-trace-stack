e86e65e3ebc2db8f22261a74e3810e6c328a3ddc
Yeyi He <81464025+hoyipyik@users.noreply.github.com>
2025-04-25 15:17:40 +0200

    add json file downloader

--- src/trace/ui/components/topbar/traceLoader.js
+++ src/trace/ui/components/topbar/traceLoader.js
@@ -22,11 +22,19 @@ export const traceLoader = () => {
                 const parsedXml = await xmlFileReader(file);
                 console.log("context", window.context);
                 const { cascadeTree, nodeMap, nodes: parsedNodes, rootNode,
-                    edges: parsedEdges, cytoscapeStyles: style, packageColorMap, idRangeByThreadMap  } = callTreeParser(parsedXml);
+                    edges: parsedEdges, cytoscapeStyles: style, packageColorMap, idRangeByThreadMap } = callTreeParser(parsedXml);
                 // const graph = { nodes: parsedNodes, edges: parsedEdges, style };
                 // renderCallTree(graph);
                 loadFlameGraphPlugin(cascadeTree, nodeMap, rootNode, packageColorMap, idRangeByThreadMap);
-                // updateTraceNodesOnClassviz();
+                // download cascadeTree as json
+                // const cascadeTreeJson = JSON.stringify(cascadeTree, null, 2); // Added null and 2 for pretty printing
+                // const blob = new Blob([cascadeTreeJson], { type: 'application/json' });
+                // const url = URL.createObjectURL(blob);
+                // const a = document.createElement('a');
+                // a.href = url;
+                // a.download = 'cascadeTree.json';
+                // a.click();
+                // URL.revokeObjectURL(url);
 
             }
         });
