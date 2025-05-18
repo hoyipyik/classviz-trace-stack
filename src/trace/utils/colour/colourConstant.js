4396356b740b9763830a6e462e7914f201529495
Yeyi He <81464025+hoyipyik@users.noreply.github.com>
2025-05-18 16:51:01 +0200

    change: extract colour map

---
+++ src/trace/utils/colour/colourConstant.js
@@ -0,0 +1,32 @@
+// Package colors palette - 50 distinguishable colors
+export const PACKAGE_COLORS = [
+  "#FFFFB3", // Pale Yellow
+  "#E41A1C", // Bright Red
+  "#377EB8", // Steel Blue
+  "#4DAF4A", // Medium Green
+  "#984EA3", // Purple
+  "#FF7F00", // Orange
+  "#A65628", // Brown
+  "#F781BF", // Pink
+  "#8DD3C7", // Mint
+  "#ECC94B", //  Yellow
+  "#BEBADA", // Lavender
+  "#FB8072", // Salmon
+  "#80B1D3", // Light Blue
+  "#FDB462", // Light Orange
+  "#B3DE69", // Lime Green
+  "#FCCDE5", // Light Pink
+  "#BC80BD", // Medium Purple
+  "#CCEBC5", // Pale Green
+  "#FFED6F", // Pale Gold
+  "#E41E36", // Crimson
+  "#00A087", // Teal
+  "#FF00FF", // Magenta
+  "#0072B2", // Dark Blue
+  "#009E73", // Sea Green
+  "#56B4E9", // Sky Blue
+  "#CC79A7", // Rose
+  "#D55E00", // Vermilion
+  "#332288", // Indigo
+  "#000000"  // Black
+];
